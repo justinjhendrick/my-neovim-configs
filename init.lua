49c6cb96e9bc5d1c341220e2ef04ff98a77cad52
Justin Hendrick <justinjhendrick@gmail.com>
2026-02-09 12:03:21 -0800

    Syncronize with init.lua from work computer

--- init.lua
+++ init.lua
@@ -1,26 +1,31 @@
 require("config.lazy")
 
-vim.opt.number = true
-
--- whitespace
+-- whitespace within the code
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
-
 -- show tabs as »
 vim.o.list = true
 vim.o.listchars = 'tab:» '
 
+-- whitespace around the code
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "121"
+
 vim.o.guifont = "FiraCode Nerd Font Mono"
 
+-- Rust specific options
 vim.lsp.enable("rust-analyzer")
 
+-- Avoid diagnostic overload. Set minimum severities
 vim.diagnostic.config({
     severity_sort = true,
-    underline = { severity = vim.diagnostic.severity.ERROR },
-    signs = { severity = vim.diagnostic.severity.ERROR },
-    jump = { severity = vim.diagnostic.severity.ERROR },
-    virtual_text = { severity = vim.diagnostic.severity.ERROR },
-    update_in_insert = false, -- Optional: do not update diagnostics while in insert mode
-    underline = true, -- Optional: underline diagnostics in the code
+    float =        { severity = { min = vim.diagnostic.severity.ERROR } },
+    signs =        { severity = { min = vim.diagnostic.severity.WARN } },
+    virtual_text = { severity = { min = vim.diagnostic.severity.ERROR } },
+    jump =         { severity = { min = vim.diagnostic.severity.ERROR } },
+    update_in_insert = false,
+    underline = true,
 })
